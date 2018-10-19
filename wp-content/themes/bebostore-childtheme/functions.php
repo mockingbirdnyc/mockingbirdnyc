a899c41b5910a6fe9a24b6d1a8a889d83f9b4c3a
Brian McMillen <brian.mcmillen@explorelearning.com>
2018-10-18 21:06:41 -0400

    Add the photoswipe javascript library to the site

--- wp-content/themes/bebostore-childtheme/functions.php
+++ wp-content/themes/bebostore-childtheme/functions.php
@@ -9,3 +9,13 @@ add_action( 'wp_enqueue_scripts', 'bebochild_theme_style' );
 
 
 /* Insert custom functions below */
+function photo_swipe_scripts() {
+	wp_enqueue_style( 'photoswipe-style', get_stylesheet_directory_uri() . '/photoswipe/photoswipe.css' );
+	wp_enqueue_style( 'photoswipe-default-skin', get_stylesheet_directory_uri() . '/photoswipe/default-skin/default-skin.css' );
+	wp_enqueue_script( 'photoswipe-ui-default-script', get_stylesheet_directory_uri() . '/photoswipe/photoswipe-ui-default.js' );
+	wp_enqueue_script( 'photoswipe-ui-default-min-script', get_stylesheet_directory_uri() . '/photoswipe/photoswipe-ui-default.min.js' );
+	wp_enqueue_script( 'photoswipe-script', get_stylesheet_directory_uri() . '/photoswipe/photoswipe.js' );
+	wp_enqueue_script( 'photoswipe-min-script', get_stylesheet_directory_uri() . '/photoswipe/photoswipe.min.js' );
+	wp_enqueue_script( 'photoswipe-main-script', get_stylesheet_directory_uri() . '/photoswipe/main.js', array( 'jquery' ) );
+}
+add_action( 'wp_enqueue_scripts', 'photo_swipe_scripts' );
---
+++ wp-content/themes/bebostore-childtheme/photoswipe/main.js
@@ -0,0 +1,66 @@
+window.onload = function () {
+  // need to add the photoswipe element to the site
+  jQuery(`
+  <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="pswp__bg"></div>
+    <div class="pswp__scroll-wrap">
+      <div class="pswp__container">
+        <div class="pswp__item"></div>
+        <div class="pswp__item"></div>
+        <div class="pswp__item"></div>
+      </div>
+      <div class="pswp__ui pswp__ui--hidden">
+        <div class="pswp__top-bar">
+          <div class="pswp__counter"></div>
+          <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+          <button class="pswp__button pswp__button--share" title="Share"></button>
+          <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+          <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+          <div class="pswp__preloader">
+            <div class="pswp__preloader__icn">
+              <div class="pswp__preloader__cut">
+                <div class="pswp__preloader__donut"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+          <div class="pswp__share-tooltip"></div>
+        </div>
+        <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+        <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+        <div class="pswp__caption">
+          <div class="pswp__caption__center"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `).appendTo(document.body);
+
+  pswpElement = document.querySelectorAll('.pswp')[0];
+
+  var $imgs = jQuery('.book-detail .woocommerce-product-gallery__image img');
+  var items = $imgs.map(function() {
+    var $el = jQuery(this);
+    return {
+      src: $el.data('src'),
+      w: $el.data('large_image_width'),
+      h: $el.data('large_image_height')
+    }
+  })
+
+  console.log(items)
+
+  var options = { index: 0 };
+
+  // Initializes and opens PhotoSwipe
+
+  $imgs.on('click', function(e) {
+    console.log('click!!!', e)
+    e.preventDefault();
+    e.stopPropagation();
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+
+    gallery.init();
+  })
+};
